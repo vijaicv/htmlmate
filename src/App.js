b12511a6bf8556a25c6e833c2c16bbdfae1c5781
vijaicv <vijaicv@gmail.com>
2018-01-26 18:21:18 +0530

    started developement........

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Displayscreen from './components/Displayscreen';
+
+class App extends Component {
+  render() {
+    return (
+        <Displayscreen/>
+    )
+  }
+}
+
+export default App;
---
+++ src/components/Displayscreen.js
@@ -0,0 +1,12 @@
+import React,{Component} from 'react';
+
+class Displayscreen extends Component{
+  render(){
+    return(
+      <div>
+         <button>Button</button>
+      </div>
+    );
+  }
+}
+export default Displayscreen;
